c99558475cc29d0725d1363880c9c3288de50ebd
Alice Freda <alicefreda@gmail.com>
2019-10-21 23:07:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,31 +2,30 @@ require 'pry'
 class School
   attr_accessor :name
   attr_reader :roster
-  ROSTER = {}
+  ROSTER = Hash.new
   def initialize(school)
     @school = school
     @roster = ROSTER
   end
   # def roster
-  #   @roster
+  #   @roster = ROSTER
   # end
   def add_student(student_name, grade)
-    # if ROSTER.has_key?("#{grade}")
-    #   ROSTER["#{grade}"] << "#{student_name}"
-    # else
-      # ROSTER.map do |grade, student_array|
-      #   if grade == "#{grade}"
-        if ROSTER.has_key?("grade") == false
-          ROSTER[grade] = []
-          ROSTER[grade] << "#{student_name}"
-        else
-          ROSTER[grade] << "#{student_name}" unless ROSTER.has_value?("student_name")
-        end
-      ROSTER
-      binding.pry
-      # end
-    # end
 
+        if @roster.has_key?("#{grade}") == false
+          @roster[grade] = []
+          @roster[grade] << "#{student_name}"
+        else
+          @roster[grade] << "#{student_name}"
+        end
+      @roster
+
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster[grade].each { |grade, students| students.sort}
   end
 
 end
